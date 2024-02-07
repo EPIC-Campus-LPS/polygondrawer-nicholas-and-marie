5dc64010280891e0c5ea9db8a2b0246da6673c44
nicholas-shortlastname <nicholasu750@lpsk12.org>
2024-02-07 12:00:19 -0700

    halfway through Boundary concepts

--- src/view/Application.java
+++ src/view/Application.java
@@ -3,14 +3,16 @@ package view;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
 public class Application extends JFrame{
 
-    private JPanel contentPane;
-    private JMenu Polygon;
-    private JMenu Edit;
-    private JMenuItem Reset;
-    private JMenuItem Undo;
+    private PolygonDrawer contentPane;
+    private JMenu menu_Polygon;
+    private JMenu menu_Edit;
+    private JMenuItem item_Reset;
+    private JMenuItem item_RemoveLast;
     private JMenuBar menuBar;
 
     public Application() {
@@ -18,22 +20,24 @@ public class Application extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100,100,450,300);
 
-        JMenuBar menuBar = new JMenuBar();
+        menuBar = new JMenuBar();
         setJMenuBar(menuBar);
 
-        JMenu mnPolygon = new JMenu("Polygon");
-        menuBar.add(mnPolygon);
+        menu_Polygon = new JMenu("Polygon");
+        menuBar.add(menu_Polygon);
 
-        JMenuItem mniReset = new JMenuItem("Reset");
-        mnPolygon.add(mniReset);
+        item_Reset = new JMenuItem("Reset");
+        item_Reset.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_MASK));
+        menu_Polygon.add(item_Reset);
 
-        JMenu mnEdit = new JMenu("Edit");
-        menuBar.add(mnEdit);
+        menu_Edit = new JMenu("Edit");
+        menuBar.add(menu_Edit);
 
-        JMenuItem mniUndo = new JMenuItem("Remove Last");
-        mnEdit.add(mniUndo);
+        item_RemoveLast = new JMenuItem("Remove Last");
+        item_RemoveLast.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, 0));
+        menu_Edit.add(item_RemoveLast);
 
-        contentPane = new JPanel();
+        contentPane = new PolygonDrawer();
         contentPane.setBorder(new EmptyBorder(5,5,5,5));
         contentPane.setLayout(new BorderLayout(0,0));
         setContentPane(contentPane);
